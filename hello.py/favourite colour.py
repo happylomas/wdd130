8bad3aed8156d94d76296eefc858429d9901aa24
happylomas <prince56ebere@gmail.com>
2023-01-16 22:25:38 +0100

    Adding2

--- hello.py/favourite colour.py
+++
@@ -1,4 +0,0 @@
-favourite_colour = input('what is your favourite colour')
-print('favourite_colour')
-print ('Green')
-print()
--- hello.py/first_name = 'prince'.py
+++
@@ -1,6 +0,0 @@
-first_name = 'prince'
-print(first_name)
-last_name = 'ebere'
-print(last_name)
-print (first_name + last_name + male)
-
